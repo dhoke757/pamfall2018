67dcd7d62297396ebeff0207d709d22439864982
David Hoke <davidhtroop62@gmail.com>
2018-12-08 16:49:39 -0500

    added teams

--- pamf18/backend/src/services/index.js
+++ pamf18/backend/src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const pitchers = require('./pitchers/pitchers.service.js');
+const teams = require('./teams/teams.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(pitchers);
+  app.configure(teams);
 };
